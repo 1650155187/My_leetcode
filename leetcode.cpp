79785ad79aafdb44291a1c2c140c1e3098de0ad7
1650155187 <36977859+1650155187@users.noreply.github.com>
2021-03-26 19:32:17 +0800

    Update leetcode.cpp

--- leetcode.cpp
+++ leetcode.cpp
@@ -1,1 +1,32 @@
-test submit
+class Solution {
+public:
+    void dfs(const string &s, int k, int pos, int &total, int &len, int sum)
+    {
+        if (pos > s.length()) {
+            return;
+        }
+        if (pos == s.length() && k == 0) {
+            total++;
+            len = sum;
+            return;
+        }
+        if (s[pos] >= '0' && s[pos] <= '9') {
+            int cur = k * 10 + s[pos] - '0';
+            if (cur > 0) {
+                dfs(s, 0, pos + cur + 1, total, len, sum + cur);
+                dfs(s, cur, pos + 1, total, len, sum);
+            }
+        }
+    }
+
+    int GetLength(string encodedString)
+    {
+        int total = 0;
+        int len = -1;
+        dfs(encodedString, 0, 0, total, len, 0);
+        if (total == 1) {
+            return len;
+        }
+        return -1;
+    }
+};
